82c07ae00481edc07b3898f6084a8aff3862e7e1
Sampad Biswas <144654539+SamSpiddY@users.noreply.github.com>
2024-08-29 00:31:55 +0530

    Task 02 Keywords and expressions

--- Main.java
+++ Main.java
@@ -1,15 +1,62 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        boolean GameOver= true;
+        int Score = 800;
+        int CompleteLevel = 5;
+        int Bonus = 100;
+
+        if (Score < 5000 && Score > 1000){
+            System.out.println("The Score was less than 5000 and greater then 1000");
+        }else if (Score < 1000) {
+            System.out.println("Your score was less then 1000");
+        }else {
+            System.out.println("Got here");
         }
+// **************************************** This line was written for the Hacker rank coding ****************
+//        Scanner sc = new Scanner (System.in);
+//
+//        int n = sc.nextInt();
+//
+//        if(n % 2 == 1){
+//            System.out.println("Weird");
+//        }
+//        else if(n % 2 == 0 && n > 2 && n < 5){
+//            System.out.println("Not Weird");
+//        }else if(n % 2 == 0 && n > 6 && n < 20){
+//            System.out.println("Weird");
+//        }else {
+//            System.out.println("Not Weird");
+//        }
+
+//        scanner();
+//        printMegaBytesAndKiloBytes(2500);
+
     }
+                                 /***********To give something input in java programming*************/
+//    public static void scanner(){
+//        Scanner sam = new Scanner(System.in);
+//        String a = sam.next();
+//        int b = sam.nextInt();
+//        double c = sam.nextDouble();
+//
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//
+//    }
+
+    /********************Udemy Codding Practice************************/
+//    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+//        int megaBytes = (kiloBytes / 1024);
+//        int remainingKB = (kiloBytes % 1024);
+//
+//        if (kiloBytes < 0){
+//            System.out.println("Invalid Value");
+//        }else{
+//            System.out.println(kiloBytes + "KB = " + megaBytes + "MB and " + remainingKB + " KB");
+//        }
+//
+//    }
+    
 }
