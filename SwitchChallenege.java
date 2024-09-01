85659a11d37d7175d69718ad2103f24b28754dab
Sampad Biswas <144654539+SamSpiddY@users.noreply.github.com>
2024-09-01 23:24:53 +0530

    Task04 Switch Challenge

---
+++ SwitchChallenege.java
@@ -0,0 +1,26 @@
+public class SwitchChallenege {
+    public static void main(String[] args) {
+        char NATO = 'E';
+
+        switch (NATO){
+            case 'A':
+                System.out.println(NATO + " = Able");
+                break;
+            case 'B':
+                System.out.println(NATO + " = Baker");
+                break;
+            case 'C':
+                System.out.println(NATO + " = Charlie");
+                break;
+            case 'D':
+                System.out.println(NATO + " = Dog");
+                break;
+            case 'E':
+                System.out.println(NATO + " = Easy");
+                break;
+            default:
+                System.out.println(NATO + " is Not Found");
+                break;
+        }
+    }
+}
