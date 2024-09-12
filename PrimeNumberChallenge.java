71eb55d4a71397b2175d782328e7afd3ea279392
Sampad Biswas <144654539+SamSpiddY@users.noreply.github.com>
2024-09-12 23:57:07 +0530

    Update PrimeNumberChallenge.java

--- PrimeNumberChallenge.java
+++ PrimeNumberChallenge.java
@@ -2,7 +2,7 @@ public class PrimeNumberChallenge {
     public static void main(String[] args) {
         System.out.println("This Number is" + ( isPrimeNumber(9973) ? "" : " not" ) + " a prime number.");
     }
-
+//prime number check with method with paramitter
     public static boolean isPrimeNumber(int number){
         if (number <= 2){
             return (number == 2);
