35c217ae5590e7e5f8cabb3e53d34df87f04ae4a
Sampad Biswas <144654539+SamSpiddY@users.noreply.github.com>
2024-09-22 22:11:53 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        int SwitchValue = 3;
 
-        System.out.println("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        switch (SwitchValue){
+            case 1:
+            System.out.println("The Switch Value is 1");
+            break;
+            case 2:
+                System.out.println("The Switch value is 2");
+                break;
+            default:
+                System.out.println("The Switch value is more then 1 and 2");
         }
+//        switch (SwitchValue) {
+//            case 1 -> System.out.println("The Switch Value is 1");
+//            case 2 -> System.out.println("The Switch value is 2");
+//            default -> System.out.println("The Switch value is more then 1 and 2");
+//        }
     }
 }
